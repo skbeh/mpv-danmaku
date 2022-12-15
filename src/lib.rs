d6ca8108ca36ecdda1bb5cf4836911f88245166d
skbeh <60107333+skbeh@users.noreply.github.com>
2022-12-15 18:30:14 +0800

    make rustc happy

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,34 @@
 use libmpv::{events::Event, Mpv};
 use libmpv_sys::mpv_handle;
 use std::{
-    env::temp_dir,
-    fs::{remove_file, File},
+    fs::File,
     io::Write,
+    mem::{transmute, ManuallyDrop},
     process::Command,
     ptr::NonNull,
     sync::atomic::AtomicBool,
 };
+use tempfile::{tempdir, TempDir};
 use url::Url;
+
+#[allow(dead_code)]
+struct InnerMpv {
+    ctx: NonNull<libmpv_sys::mpv_handle>,
+    events_guard: AtomicBool,
+}
 
 #[no_mangle]
 extern "C" fn mpv_open_cplugin(handle: *mut mpv_handle) -> std::os::raw::c_int {
-    let mpv = Mpv {
+    let pub_mpv = InnerMpv {
         ctx: NonNull::new(handle).unwrap(),
         events_guard: AtomicBool::new(false),
     };
+    let mpv = ManuallyDrop::new(unsafe { transmute::<InnerMpv, Mpv>(pub_mpv) });
     let mut event_context = mpv.create_event_context();
-    let mut path_vec: Vec<String> = Vec::new();
 
     loop {
         match event_context.wait_event(-1.).unwrap().unwrap() {
             Event::FileLoaded => {
-                println!("{}", mpv.get_property::<String>("path").unwrap());
                 match Url::parse(&mpv.get_property::<String>("path").unwrap()) {
                     Err(_) => continue,
                     Ok(url) => {
@@ -34,25 +40,21 @@ extern "C" fn mpv_open_cplugin(handle: *mut mpv_handle) -> std::os::raw::c_int {
 
                 remove_xml_sub(&mpv);
 
-                let temp_directory = temp_dir();
-                let temp_path_buf = temp_directory
-                    .join(mpv.get_property::<String>("filename").unwrap())
-                    .with_extension("ass");
-                path_vec.push(temp_path_buf.to_str().unwrap().to_owned());
-                let mut temp_file = File::create(temp_path_buf).unwrap();
+                let (temp_dir, mut temp_file);
+                match create_temp_file(mpv.get_property::<String>("filename").unwrap()) {
+                    None => continue,
+                    Some(temp_tuple) => (temp_dir, temp_file) = temp_tuple,
+                }
 
-                let subtitle = get_danmaku_ass(&mpv.get_property("path").expect("test0")).unwrap();
+                let subtitle = get_danmaku_ass(&mpv.get_property("path").unwrap()).unwrap();
                 temp_file.write_all(&subtitle).unwrap();
 
                 mpv.set_property("sub-auto", "exact").unwrap();
-                mpv.set_property("options/sub-file-paths", temp_directory.to_str().unwrap())
+                mpv.set_property("options/sub-file-paths", temp_dir.path().to_str().unwrap())
                     .unwrap();
                 mpv.command("rescan-external-files", &["reselect"]).ok();
             }
             Event::Shutdown => {
-                path_vec.iter().for_each(|path| {
-                    remove_file(path).ok();
-                });
                 return 0;
             }
             _ => {}
@@ -83,8 +85,16 @@ fn remove_xml_sub(mpv: &Mpv) {
     }
 }
 
+fn create_temp_file(filename: String) -> Option<(TempDir, File)> {
+    let temp_directory = tempdir().ok()?;
+    let temp_path_buf = temp_directory.path().join(filename).with_extension("ass");
+    Some((temp_directory, File::create(temp_path_buf).ok()?))
+}
+
 fn get_danmaku_ass(path: &String) -> Option<Vec<u8>> {
-    let output = Command::new("danmu2ass").args(["-o", "-", path]).output();
+    let output = Command::new("danmu2ass")
+        .args(["-o", "-", path])
+        .output();
     match output {
         Ok(output) => {
             println!("{}", String::from_utf8(output.stderr).unwrap());
